96d8eed206f0fdf972437c462637b0230797c5fc
devshine2015 <nikita.afanasev2020@gmail.com>
2021-07-08 01:15:53 +0300

    test again 11

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { /*Menu,*/ Button } from "antd";
-// import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 
 // import NavbarList from "../../const/navbar";
 
